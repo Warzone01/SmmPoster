8d7e5b17b784afccec307203e2ad546953d09897
Warzone01 <kirill.trumpeter@gmail.com>
2021-01-17 07:54:13 +0300

    add miscellaneous

--- app/src/main/java/com/kirdevelopment/smmposter/activities/CreatePostActivity.kt
+++ app/src/main/java/com/kirdevelopment/smmposter/activities/CreatePostActivity.kt
@@ -1,22 +1,44 @@
 package com.kirdevelopment.smmposter.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.kirdevelopment.smmposter.R
 
 class CreatePostActivity : AppCompatActivity() {
     private lateinit var cancelBtn: ImageView
+    private lateinit var behavior: View
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
 
         cancelBtn = findViewById(R.id.createPostCancel)
+        behavior = findViewById(R.id.behavior)
 
         cancelBtn.setOnClickListener {
             finish()
+        }
+
+        initMiscellaneous()
+
+    }
+
+
+    private fun initMiscellaneous() {
+        val layoutMiscellaneous: LinearLayout = findViewById(R.id.layoutMiscellaneous)
+        val bottomSheetBehavior: BottomSheetBehavior<LinearLayout> = BottomSheetBehavior.from(layoutMiscellaneous)
+
+        behavior.setOnClickListener {
+            if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            } else {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
         }
 
     }
